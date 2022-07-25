dc5181899892e94b60b87fa2d886b5996110b0c4
Nguyễn Thị Kim Ánh <109960444+ntk-anh@users.noreply.github.com>
2022-07-25 23:44:40 +0700

    Update HomeController.java

--- src/main/java/com/cybersoft/cineflix2/controller/HomeController.java
+++ src/main/java/com/cybersoft/cineflix2/controller/HomeController.java
@@ -17,9 +17,9 @@ public class HomeController {
 		
 		int soA=15;
 		int soB=10;
-		int soC=20;
+		int soC=15;
 		int soD=10;
-		
+		int soF=9;
 		int tong=tinhTong(soA, soB);
 		
 		andView.addObject("tong", tong);
